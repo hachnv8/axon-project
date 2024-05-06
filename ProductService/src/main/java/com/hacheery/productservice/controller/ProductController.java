08b1726e78a5e2a443f0db58abb93648c2f5c8f1
hachnv8 <hachnv8@gmail.com>
2024-05-06 14:32:30 +0700

    create and update api gateway

--- ProductService/src/main/java/com/hacheery/productservice/controller/ProductController.java
+++ ProductService/src/main/java/com/hacheery/productservice/controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.hacheery.productservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final Environment environment;
 
     @PostMapping
     public String createProduct() {
@@ -13,7 +17,7 @@ public class ProductController {
 
     @GetMapping
     public String getProducts() {
-        return "Product list";
+        return "Product list" + environment.getProperty("local.server.port");
     }
 
     @PutMapping
