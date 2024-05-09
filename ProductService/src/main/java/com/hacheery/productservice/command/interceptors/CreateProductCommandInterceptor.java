9560ebf74e167236a8311054022cea44a896a0f6
hachnv8 <hachnv8@gmail.com>
2024-05-09 22:18:45 +0700

    add message dispatch interceptor to project

---
+++ ProductService/src/main/java/com/hacheery/productservice/command/interceptors/CreateProductCommandInterceptor.java
@@ -0,0 +1,43 @@
+package com.hacheery.productservice.command.interceptors;
+
+import com.hacheery.productservice.command.CreateProductCommand;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.messaging.MessageDispatchInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.BiFunction;
+
+@Component
+@RequiredArgsConstructor
+public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateProductCommandInterceptor.class);
+
+    @Nonnull
+    @Override
+    public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(@Nonnull List<? extends CommandMessage<?>> messages) {
+
+        return (index, command) -> {
+            LOGGER.info("Handling command {}", command.getPayloadType());
+
+            if(CreateProductCommand.class.equals(command.getPayloadType())) {
+                CreateProductCommand createProductCommand = (CreateProductCommand) command.getPayload();
+
+                if(createProductCommand.getPrice().compareTo(new BigDecimal(0)) < 0) {
+                    throw new IllegalArgumentException("Price must be greater than zero");
+                }
+
+                if(createProductCommand.getTitle() == null || createProductCommand.getTitle().isEmpty()) {
+                    throw new IllegalArgumentException("Title cannot be null or empty");
+                }
+
+            }
+            return command;
+        };
+    }
+}
